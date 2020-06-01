956d03dca27a16010256d4aff08f7dcb170845ba
Ilya <inidzelsky@gmail.com>
2020-06-01 12:26:58 +0300

    Create InvalidCoordsOrderException exception

--- Polyhedrons/Polygon.cs
+++ Polyhedrons/Polygon.cs
@@ -1,8 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 
 namespace Polyhedrons
 {
+    public class InvalidCoordsOrderException : Exception
+    {
+        public InvalidCoordsOrderException(string message) :
+            base(message)
+        {
+        }
+    }
+
     public class Polygon
     {
         private List<Coords> _coords;
@@ -12,25 +21,25 @@ namespace Polyhedrons
             _coords = coords;
         }
 
-        public virtual double GetPerimeter()
+        public virtual double GetPerimeter() // #TODO Handle 0 - 1 vertexes exception
         {
             double perimeter = 0;
             int coordsCount = _coords.Count;
 
             if (coordsCount < 2)
                 return perimeter;
-            
+
             for (int i = 0; i < coordsCount - 1; i++)
             {
                 perimeter += GetLength(_coords[i], _coords[i + 1]);
             }
 
             perimeter += GetLength(_coords[coordsCount - 1], _coords[0]);
-            
+
             return perimeter;
         }
 
-        public double GetSquare()
+        public double GetSquare() // #TODO Handle 0 - 2 vertexes exception
         {
             double square = 0;
             int coordsCount = _coords.Count;
@@ -46,11 +55,14 @@ namespace Polyhedrons
 
             square += _coords[coordsCount - 1].X * _coords[0].Y;
             square -= _coords[coordsCount - 1].Y * _coords[0].X;
+            square /= 2;
 
-            return square / 2;
+            ValidateSquare(square);
+            return square;
         }
-        
-        public virtual int GetVertexes()
+
+
+        public virtual int GetVertexes() // #TODO Handle 0 vertexes exception
         {
             return _coords.Count;
         }
@@ -58,6 +70,12 @@ namespace Polyhedrons
         public List<Coords> GetCoords()
         {
             return _coords;
+        }
+
+        private void ValidateSquare(double square)
+        {
+            if (square < 0)
+                throw new InvalidCoordsOrderException("Coords were set in an inappropriate way");
         }
 
         private double GetLength(Coords c1, Coords c2)
