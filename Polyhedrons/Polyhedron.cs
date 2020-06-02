59839fb85a088827bf2f7401f2fd9212049ab1f9
Ilya <inidzelsky@gmail.com>
2020-06-02 14:15:54 +0300

    Abstract Polyhedron class implement

--- Polyhedrons/Polyhedron.cs
+++ Polyhedrons/Polyhedron.cs
@@ -1,7 +1,60 @@
 namespace Polyhedrons
 {
+    public class Prism : Polyhedron
+    {
+        
+        public override double GetVolume()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetApexes()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetEdges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetBrinks()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
+    public class Cube : Polyhedron
+    {
+        
+    }
+
+    public class Parallelepiped : Polyhedron
+    {
+        
+    }
+
+    public class Pyramid : Polyhedron
+    {
+        
+    }
+    
     public abstract class Polyhedron
     {
+        private Polygon _base;
+
+        public double GetBaseArea()
+        {
+            return _base.GetArea();
+        }
+
+        public abstract double GetVolume();
+
+        public abstract int GetApexes();
+
+        public abstract int GetEdges();
+
+        public abstract int GetBrinks();
         
     }
 }
